93d5e8702fea1552ed05d099c70d00ebde17427f
Anuj Kumar RoY <106427006+anujkumarroy@users.noreply.github.com>
2024-04-18 23:58:40 +0530

    Update main.py

--- SHIELD3/modules/main.py
+++ SHIELD3/modules/main.py
@@ -52,15 +52,15 @@ NYKAA = [
 
 start_txt = """<b> ❍ ʜɪɪ ʙᴀʙʏ, ɴɪᴄᴇ ᴛᴏ ᴍᴇᴇᴛ ᴜʜʜ !\n━━━━━━━━━━━━━━━━━━━━━━\n\n❍ ɪ ᴀᴍ ɢʀᴏᴜᴘ ᴘʀᴏᴛᴇᴄᴛɪᴏɴ ᴄᴏᴘʏʀɪɢʜᴛ ʙᴏᴛ.\n\n❍ ɪ ʜᴀᴠᴇ ɢʀᴏᴜᴘ ᴘʀᴏᴛᴇᴄᴛɪᴏɴ ғᴇᴀᴛᴜʀᴇs.\n\n❍ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ ʀ ᴏ ʏ - ᴇ ᴅ ɪ ᴛ x ࿐ </b>"""
 
-@app.on_message(filters.command("m"))
+@app.on_message(filters.command("start"))
 async def start(_, msg):
     buttons = [
         [ 
           InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
         ],
         [
-          InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data="GODFATHER_back"),
-          InlineKeyboardButton("ʜᴇʟᴘ", callback_data="GODFATHER_back")
+          InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data="nykaa_back"),
+          InlineKeyboardButton("ʜᴇʟᴘ", callback_data="roy_back")
         ],
         [
           InlineKeyboardButton("sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ", callback_data="gib_source"),
@@ -76,16 +76,16 @@ async def start(_, msg):
 
 gd_buttons = [              
         [
-            InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ", url="https://t.me/Op_Godfather"),
-            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/GODFADHER_BOTS"),    
+            InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ", url="https://t.me/roy_editx"),
+            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/THE_FRIENDZ"),    
         ]
         ]
 # ------------------------------------------------------------------------------- #
 
 ROY_BTN = [              
         [
-            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/GODFADHER_BOTS"),
-            InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ", url="https://t.me/Op_Godfather"),    
+            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/the_friendz"),
+            InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ", url="https://t.me/roy_editx"),    
         ]
 ]
 # ------------------------------------------------------------------------------- #
@@ -99,8 +99,8 @@ async def cutebackbutton(client, callback_query: CallbackQuery, _):
               InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
             ],
             [
-              InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data="GODFATHER_back"),
-              InlineKeyboardButton("ʜᴇʟᴘ", callback_data="GODFATHER_back")
+              InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data="nykaa_back"),
+              InlineKeyboardButton("ʜᴇʟᴘ", callback_data="roy_back")
             ],
             [
               InlineKeyboardButton("sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ", callback_data="gib_source"),
@@ -115,7 +115,7 @@ async def cutebackbutton(client, callback_query: CallbackQuery, _):
 
     
 
-@app.on_callback_query(filters.regex("GODFATHER_back"))
+@app.on_callback_query(filters.regex("nykaa_back"))
 async def nykaa_back(_, query: CallbackQuery):
     await query.message.edit_caption(ABOUT_STRING,
                                     reply_markup=InlineKeyboardMarkup(gd_buttons),)
@@ -129,7 +129,7 @@ ABOUT_STRING = """**✦ ɪ ʜᴀᴠᴇ ᴍᴏsᴛ ᴘᴏᴡᴇʀғᴜʟʟ ɢʀ�
 
 # -------------------------------------------------------------------------------------
 
-HELP_STRING = """**✽ ᴏᴡɴᴇʀ/sᴜᴅᴏ ᴜsᴇʀ ᴄᴍᴅs ⏤͟͟͞͞★\n\n❍ /bcast ➠ ʙʀᴏᴀᴅᴄᴀsᴛ ᴀɴʏ ᴍᴇssᴀɢᴇ.\n❍ /announce ➠ ʀᴇᴘʟʏ ᴀɴʏ ᴍᴇssᴀɢᴇ ᴛᴏ ᴀɴɴᴏᴜɴᴄᴇ.\n\n✽ ᴀʟʟ ᴜsᴇʀs  ᴄᴏᴍᴍᴀɴᴅs ⏤͟͟͞͞★\n\n❍ /start ➠ sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ.\n❍ /ping ➠ ᴄʜᴋ ʙᴏᴛ ᴘɪɴɢ sᴛᴀᴛs.\n❍ /repo ➠ ɢᴇᴛ ʙᴏᴛ ʀᴇᴘᴏ.\n\n❍ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠  ʀ ᴏ ʏ - SAM**"""
+HELP_STRING = """**✽ ᴏᴡɴᴇʀ/sᴜᴅᴏ ᴜsᴇʀ ᴄᴍᴅs ⏤͟͟͞͞★\n\n❍ /bcast ➠ ʙʀᴏᴀᴅᴄᴀsᴛ ᴀɴʏ ᴍᴇssᴀɢᴇ.\n❍ /announce ➠ ʀᴇᴘʟʏ ᴀɴʏ ᴍᴇssᴀɢᴇ ᴛᴏ ᴀɴɴᴏᴜɴᴄᴇ.\n\n✽ ᴀʟʟ ᴜsᴇʀs  ᴄᴏᴍᴍᴀɴᴅs ⏤͟͟͞͞★\n\n❍ /start ➠ sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ.\n❍ /ping ➠ ᴄʜᴋ ʙᴏᴛ ᴘɪɴɢ sᴛᴀᴛs.\n❍ /repo ➠ ɢᴇᴛ ʙᴏᴛ ʀᴇᴘᴏ.\n\n❍ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠  ʀ ᴏ ʏ - ᴇ ᴅ ɪ ᴛ x ࿐**"""
 
 # ------------------------------------------------------------------------------- #
 
@@ -142,7 +142,7 @@ EVAA = [
 # ------------------------------------------------------------------------------- #
 
 
-@app.on_callback_query(filters.regex("GODFATHER_back"))
+@app.on_callback_query(filters.regex("roy_back"))
 async def roy_back(_, query: CallbackQuery):
     await query.message.edit_caption(HELP_STRING,
                                     reply_markup=InlineKeyboardMarkup(ROY_BTN),)
@@ -160,8 +160,8 @@ REPO_STRING = """**
 async def start(_, msg):
     REPO_BTN = [
         [
-          InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/GODFADHER_BOTS"),
-          InlineKeyboardButton("ʀᴇᴘᴏ", url="https://t.me/GODFADHER_BOTS"),
+          InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/the_friendz"),
+          InlineKeyboardButton("ʀᴇᴘᴏ", url="https://github.com/tinaarobot/COPYRIGHT"),
           ],
     ]
     
@@ -220,9 +220,9 @@ async def handle_message(client, message):
     if any(keyword in message.text for keyword in FORBIDDEN_KEYWORDS):
         logging.info(f"✦ ᴅᴇʟᴇᴛɪɴɢ ᴍᴇssᴀɢᴇ ᴡɪᴛʜ ɪᴅ {message.id}")
         await message.delete()
-#       user_mention = msg.from_user.mention
+       user_mention = msg.from_user.mention
         await message.reply_text(f"✦ ʜᴇʏ {user_mention}, ʙᴀʙʏ ᴅᴏɴ'ᴛ sᴇɴᴅ ɴᴇxᴛ ᴛɪᴍᴇ.")
-if any(keyword in message.caption for keyword in FORBIDDEN_KEYWORDS):
+    if any(keyword in message.caption for keyword in FORBIDDEN_KEYWORDS):
         logging.info(f"✦ ᴅᴇʟᴇᴛɪɴɢ ᴍᴇssᴀɢᴇ ᴡɪᴛʜ ɪᴅ {message.id}")
         await message.delete()
         user_mention = msg.from_user.mention
@@ -262,26 +262,3 @@ async def delete_pdf_files(client, message):
 @app.on_message(filters.group & filters.document)
 async def message_handler(client, message):
     await delete_pdf_files(client, message)
-
-# ----------------------------------------
-
-@app.on_callback_query(filters.regex("gib_source"))
-async def gib_repo_callback(_, callback_query):
-    await callback_query.edit_message_media(
-        media=InputMediaVideo("https://telegra.ph/file/9235d57807362b4e227a3.mp4", has_spoiler=True),
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [close_button]
-            ]
-        ),
-        )
-close_button = InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")
-
-@app.on_callback_query(filters.regex("close"))
-async def close_menu(_, CallbackQuery):
-    try:
-        await CallbackQuery.message.delete()
-        await CallbackQuery.answer()
-    except:
-        return
-        
